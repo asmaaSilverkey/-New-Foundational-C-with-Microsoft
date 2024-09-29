7c464f08b4f1341c9f82ec934f7a262c622bffb9
asmaaSilverkey <asmaa@silverkeytech.com>
2024-09-29 16:26:21 +0300

    finishing the C# course

--- Foundational C# with Microsoft_ course/CsharpProjects/16DebugConsoleApp/Program.cs
+++ Foundational C# with Microsoft_ course/CsharpProjects/16DebugConsoleApp/Program.cs
@@ -1,5 +1,233 @@
-﻿string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+﻿// string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
 
-int studentCount = students.Length;
+// int studentCount = students.Length;
 
-Console.WriteLine("The final name is: " + students[studentCount]); //exception, arrays are zero-based ->students[studentCount - 1].
+// Console.WriteLine("The final name is: " + students[studentCount]); //exception, arrays are zero-based ->students[studentCount - 1].
+
+
+//-----------------------------------------------------------------------------------
+
+// /* 
+// This code uses a names array and corresponding methods to display
+// greeting messages
+// */
+
+// string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+// string messageText = "";
+
+// foreach (string name in names)
+// {
+//     if (name == "Sophia")
+//         messageText = SophiaMessage();
+//     else if (name == "Andrew")
+//         messageText = AndrewMessage();
+//     else if (name == "AllGreetings")
+//         messageText = SophiaMessage();
+//         messageText = messageText + "\n\r" + AndrewMessage();
+
+//     Console.WriteLine(messageText + "\n\r");
+// }
+
+// bool pauseCode = true;
+// while (pauseCode == true);
+
+// static string SophiaMessage()
+// {
+//     return "Hello, my name is Sophia.";
+// }
+
+// static string AndrewMessage()
+// {
+//     return "Hi, my name is Andrew. Good to meet you.";
+// }
+
+
+//-----------------------------------------------------------------------------------
+// 
+
+// string? readResult;
+// int startIndex = 0;
+// bool goodEntry = false;
+
+// int[] numbers = { 1, 2, 3, 4, 5 };
+
+// // Display the array to the console.
+// //Console.Clear();
+// Console.Write("\n\rThe 'numbers' array contains: { ");
+// foreach (int number in numbers)
+// {
+//     Console.Write($"{number} ");
+// }
+
+// // To calculate a sum of array elements, 
+// //  prompt the user for the starting element number.
+// Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
+// while (goodEntry == false)
+// {
+//     readResult = Console.ReadLine();
+//     goodEntry = int.TryParse(readResult, out startIndex);
+
+//     if (startIndex > 5)
+//     {
+//         goodEntry = false;
+//         Console.WriteLine("\n\rEnter an integer value between 1 and 5");
+//     }
+// }
+
+// // Display the sum and then pause.
+// Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex)}");
+
+// Console.WriteLine("press Enter to exit");
+// readResult = Console.ReadLine();
+
+// // This method returns the sum of elements n through 5
+// static int SumValues(int[] numbers, int n)
+// {
+//     int sum = 0;
+//     for (int i = n; i < numbers.Length; i++)
+//     {
+//         sum += numbers[i];
+//     }
+//     return sum;
+// }
+
+
+
+//---------------------------------------------------------------------------------
+// bool exit = false;
+// var rand = new Random();
+// int num1 = 5;
+// int num2 = 5;
+
+// do
+// {
+//     num1 = rand.Next(1, 11);
+//     num2 = num1 + rand.Next(1, 51);
+
+// } while (exit == false);
+
+//---------------------------------------------------------------------------------
+// /*  
+// This code instantiates a value and then calls the ChangeValue method
+// to update the value. The code then prints the updated value to the console.
+// */
+// int x = 5;
+
+// ChangeValue(x);
+
+// Console.WriteLine(x);
+
+// void ChangeValue(int value) 
+// {
+//     value = 10;
+// }
+//---------------------------------------------------------------------------------
+// string[] names = { "Dog", "Cat", "Fish" };
+// Object[] objs = (Object[])names;
+
+// Object obj = (Object)13;
+// objs[2] = obj; // ArrayTypeMismatchException occurs.
+// //The (Object[]) cast is used to convert the string[] array names into an Object[] array. This is possible because all types in C# implicitly inherit from the Object class.
+
+//----------------------------------------------------------------------------------
+// Exception ex = new Exception(message: "heeeerrrrrrrrrrrreeeeeeeeeeee");
+// throw new ApplicationException("An error occurred", ex);
+//----------------------------------------------------------------------------------
+
+
+
+try
+{
+    OperatingProcedure1();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Exiting application.");
+}
+
+static void OperatingProcedure1()
+{
+    string[][] userEnteredValues = new string[][]
+    {
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+    };
+
+    foreach(string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            BusinessProcess1(userEntries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.StackTrace.Contains("BusinessProcess1"))
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                }
+                else if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                    // re-throw the original exception
+                    throw;
+                }
+                else
+                {
+                    // create a new exception object that wraps the original exception
+                    throw new ApplicationException("An error occurred - ", ex);
+                }
+            }
+        }
+
+    }
+}
+
+static void BusinessProcess1(string[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+
+            checked
+            {
+                int calculatedValue = 4 / valueEntered;
+            }
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            throw invalidFormatException;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            throw unexpectedDivideByZeroException;
+
+        }
+    }
+}
+
+/*
+
+FormatException: User input values in 'BusinessProcess1' must be valid integers
+Corrective action taken in OperatingProcedure1
+DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero
+Partial correction in OperatingProcedure1 - further action required
+DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero
+Exiting application.
+
+*/
+//--------------------------------------------------------------------------------------
+
+throw new ArgumentOutOfRangeException("upperBound", "ArgumentOutOfRangeException: upper bound must be greater than lower bound.");
